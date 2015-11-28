cdd504c4c0289c9ba23e5b3a043f3ad06c94ca2a
prusio13 <prusiewicz.patryk@ideacollectors.pl>
2015-11-28 11:56:29 +0100

    2 wersje frontu
    wersja serwerowa - po wgraniu na serwerer pelny look
    wersja statyczna - mozna odrazu podejrzec
    
    // brak czcionek i ikon strzalek + down'rollu menu dzisiaj dorobie

---
+++ front/wersja_statyczna/static/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.slider1').bxSlider({
+    pager: false,
+    minSlides: 3,
+    maxSlides: 3,
+    slideWidth: 320,
+    slideMargin: 10
+  });
+
+  $('.slider2').bxSlider({
+    pager: false,
+    minSlides: 3,
+    maxSlides: 3,
+    slideWidth: 320,
+    slideMargin: 10
+  });
+});
