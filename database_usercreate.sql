6a97d603d49b317153fc2f219ef9e1a3c391dc6e
MaciejS <sobolewski.maciej@hotmail.com>
2016-01-16 14:16:29 +0100

    Usecreate

---
+++ database_usercreate.sql
@@ -0,0 +1,5 @@
+INSERT INTO Uzytkownik (login, haslo, adres, nr_telefonu, email) VALUES ('TestKosz', 'qwert123', 'testowa 2', '700880774', 'test@lololo.pl');
+INSERT INTO Koszyk (idUzytkownik, data_realizacji, zmienna_ses_kosz, status_zamowienia) VALUES (1, '9999-12-31 23:59:59', 0, 0);
+INSERT INTO listaKoszyka (idKsiazek, idKoszyka, ilość) VALUES (1, 1, 3);
+INSERT INTO listaKoszyka (idKsiazek, idKoszyka, ilość) VALUES (2, 1, 3);
+INSERT INTO listaKoszyka (idKsiazek, idKoszyka, ilość) VALUES (5, 1, 3);
